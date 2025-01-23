e83e6a4cd0124e5e94747d5a03cec5c9ab8058c2
Balthazar <balthazar.muhlstein@etu.univ-lyon1.fr>
2025-01-23 16:19:00 +0100

    Parser sans listes d'instruction

---
+++ src/CustomParser.elm
@@ -0,0 +1,58 @@
+module CustomParser exposing (..)
+
+import Parser exposing (..)
+
+-- Définition des instructions TcTurtle
+type Instruction
+    = Repeat Int (Instruction)
+    | Forward Int
+    | Left Int
+    | Right Int 
+
+-- Parse un entier
+intParser : Parser Int
+intParser =
+    Parser.int
+
+-- Parse une instruction "Forward"
+forwardParser : Parser Instruction
+forwardParser =
+    succeed Forward
+        |. symbol "Forward"
+        |. spaces
+        |= intParser
+
+-- Parse une instruction "Left"
+leftParser : Parser Instruction
+leftParser =
+    succeed Left
+        |. symbol "Left"
+        |. spaces
+        |= intParser
+
+-- Parse une instruction "Right"
+rightParser : Parser Instruction
+rightParser =
+    succeed Right
+        |. symbol "Right"
+        |. spaces
+        |= intParser
+
+-- Parse une instruction "Repeat"
+repeatParser : Parser Instruction
+repeatParser =
+    succeed Repeat
+        |. symbol "Repeat"
+        |. spaces
+        |= intParser
+        |. spaces
+        |. symbol "["
+        |= lazy (\_ -> instructionParser)
+        |. symbol "]"
+
+-- Parse une liste d'instructions
+instructionParser : Parser Instruction
+instructionParser =
+    oneOf [ forwardParser, leftParser, rightParser, repeatParser ]
+
+
