9cb62c623fdc1b137e3bf456db16ec3de3110a47
Balthazar <balthazar.muhlstein@etu.univ-lyon1.fr>
2025-01-28 15:39:48 +0100

    correction SvgDrawer

--- src/svgDrawer.elm
+++ src/svgDrawer.elm
@@ -1,6 +1,8 @@
 module SvgDrawer exposing (..)
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
+import Svg exposing (..)
+import Svg.Attributes exposing (..)
 import CustomParser exposing (..)
 import Parser exposing (..)
 
@@ -8,8 +10,6 @@ type alias State =
     { position : (Float, Float)  -- Position actuelle de la tortue (x, y)
     , angle : Float              -- Direction actuelle en degrés
     , lines : List ((Float, Float), (Float, Float)) -- Lignes tracées
-    }
-
 interpretInstructions : String -> Svg msg
 interpretInstructions input =
     let
@@ -18,6 +18,47 @@ interpretInstructions input =
         case listInstruction of
         Ok instructions ->
             let
+                finalState = treatInstructionList instructions initialState
+            in
+            stateToSvg finalState
+
+        Err _ ->
+            svg [ width "500", height "500", viewBox "-250 -250 500 500" ]
+                [ text_
+                    [ x "-200"
+                    , y "0"
+                    , fill "red"
+                    , fontSize "20"
+                    ]
+                    [ Svg.text "Parsing Error: Invalid input" ]
+                ]
+
+stateToSvg : State -> Svg msg
+stateToSvg state =
+    svg [ width "500", height "500", viewBox "-250 -250 500 500" ]
+    (List.map lineToSvg state.lines)
+
+lineToSvg : ((Float, Float), (Float, Float)) -> Svg msg
+lineToSvg ((x1, y1), (x2, y2)) =
+    line
+        [ Svg.Attributes.x1 (String.fromFloat x1)
+        , Svg.Attributes.y1 (String.fromFloat y1)
+        , Svg.Attributes.x2 (String.fromFloat x2)
+        , Svg.Attributes.y2 (String.fromFloat y2)
+        , Svg.Attributes.stroke "black"
+        , Svg.Attributes.strokeWidth "2"
+        ]
+        []
+    }
+
+interpretInstructions : String -> Svg msg
+interpretInstructions input =
+treatLeft newAngle state =
+    {state | angle = state.angle - newAngle}
+    in
+        case listInstruction of
+treatRight newAngle state =
+    {state | angle = state.angle + newAngle}
                 finalState = treatInstructionList instructions initialState
             in
             stateToSvg finalState
